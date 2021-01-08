d0d0fcbcda7b6b6e85e07f139596d10e4a501df6
leticia sales <letisaless@gmail.com>
2021-01-08 18:59:17 -0300

    fix class and add key as attr

--- lib/open_weather.rb
+++ lib/open_weather.rb
@@ -4,7 +4,13 @@ require "open_weather/version"
 module OpenWeather
   class Error < StandardError; end
 
-  def get(api_key, city)
-    return Net::HTTP.get(URI.parse("api.openweathermap.org/data/2.5/weather?q={#{city}}&appid={#{api_key}}"))
+  class Request
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def self.get(city)
+      return Net::HTTP.get(URI.parse("api.openweathermap.org/data/2.5/weather?q={#{city}}&appid={#{@api_key}}"))
+    end
   end
 end
