8f1c07cb06a92952c0d490d4d4a6c18194da87b8
leticia sales <letisaless@gmail.com>
2021-01-09 16:11:09 -0300

    add language option

--- lib/open_weather.rb
+++ lib/open_weather.rb
@@ -1,16 +1,37 @@
 require "net/http"
 require "open_weather/version"
+require "socket"
 
 module OpenWeather
   class Error < StandardError; end
 
   class Request
-    def initialize(api_key)
+    def initialize(api_key, lang = '')
       @api_key = api_key
+      @lang = lang
     end
 
     def get(city_id)
-      return Net::HTTP.get(URI.parse("api.openweathermap.org/data/2.5/weather?id=#{city_id}&appid=#{@api_key}"))
+      url = "https://api.openweathermap.org/data/2.5/weather?id=#{city_id}&appid=#{@api_key}"
+      if @lang
+        url += "&lang=#{@lang}"
+      end
+
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      headers = {
+        'Accept' => '*/*',
+        'User-Agent' => "OpenWeather Ruby Gem",
+        "Content-Type" => "application/json",
+        "Host" => Socket.gethostname,
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Connection" => "keep-alive",
+        "Access-Control-Allow-Origin" => "*"
+      }
+      data = http.get(uri.request_uri, headers)
+
+      JSON.parse(data.body)
     end
   end
 end
